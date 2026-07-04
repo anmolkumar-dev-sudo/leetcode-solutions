5459032064f7e6e68ad427161fedf4df8ef6bdcc
anmolkumar-dev-sudo <anmolofficial89@gmail.com>
2026-07-04 12:10:37 +0530

    Solution of validate a bst

---
+++ validate_a_BST.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fun(TreeNode* root,vector<int>&ans){
+        if(root==NULL)return;
+        fun(root->left,ans);
+        ans.push_back(root->val);
+        fun(root->right,ans);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int>ans;
+        fun(root,ans);
+        bool flag=true;
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i+1]<=ans[i]){
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+};
