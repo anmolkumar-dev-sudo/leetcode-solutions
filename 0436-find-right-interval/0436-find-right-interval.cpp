d980e88431d7137d7f3891dda4ccf5b4b0464cf5
anmolkumar-dev-sudo <anmolofficial89@gmail.com>
2026-09-22 13:12:31 +0530

    Time: 5 ms (74.5%), Space: 30.1 MB (48.53%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>&mat) {
         int n=mat.size();
-        vector<vector<int>>st;
+        vector<pair<int,int>>st;
         for(int i=0;i<n;i++){
             st.push_back({mat[i][0],i});
         }
@@ -13,8 +13,8 @@ public:
             int f=0,e=n-1,ans=-1;
             while(f<=e){
                 int mid=f+(e-f)/2;
-                if(st[mid][0]>=x){
-                    ans=st[mid][1];
+                if(st[mid].first>=x){
+                    ans=st[mid].second;
                     e=mid-1;
                 }
                 else f=mid+1;
